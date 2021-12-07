c40d7c91958f6870eb40f94c48447f7651c6fb50
Fredrik Wangel <fredrik.wangel@gmail.com>
2021-12-07 21:57:47 +0100

    Simplified the first part of the 2021 day 7 solution

--- src/main/kotlin/se/brainleech/adventofcode/aoc2021/Day07.kt
+++ src/main/kotlin/se/brainleech/adventofcode/aoc2021/Day07.kt
@@ -1,5 +1,6 @@
 package se.brainleech.adventofcode.aoc2021
 
+import java.util.stream.IntStream
 import kotlin.math.abs
 
 class Day07 {
@@ -8,20 +9,13 @@ class Day07 {
         val sorted = input.sorted()
         val minPosition = sorted.first()
         val maxPosition = sorted.last()
-        println("$minPosition .. $maxPosition")
 
-        var bestPosition = Int.MAX_VALUE
-        var bestFuelUsage = Int.MAX_VALUE
-        for (position in minPosition..maxPosition) {
-            val fuelUsage = sorted.sumOf { abs(position - it) }
-            if (fuelUsage < bestFuelUsage) {
-                bestFuelUsage = fuelUsage
-                bestPosition = position
+        return IntStream.rangeClosed(minPosition, maxPosition)
+            .map { position ->
+                sorted.sumOf { abs(position - it) }
             }
-        }
-
-        println("bestFuelUsage=$bestFuelUsage, bestPosition=$bestPosition")
-        return bestFuelUsage
+            .min()
+            .orElse(-1)
     }
 
     fun part2(input: List<Int>): Int {
