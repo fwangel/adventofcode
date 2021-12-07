8679bf8a9bdd7bc769712c2de7d76ea0247eefaf
Fredrik Wangel <fredrik.wangel@gmail.com>
2021-12-07 12:31:19 +0100

    Reworked using input files
    Added Common.kt and rewrote the unit tests.

---
+++ src/main/kotlin/se/brainleech/adventofcode/Common.kt
@@ -0,0 +1,43 @@
+package se.brainleech.adventofcode
+
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.util.stream.Collectors
+import java.util.stream.IntStream
+import java.util.stream.Stream
+
+fun out(value: Any, prefix: String = ""): Any {
+    print(prefix)
+    println(value)
+    return value
+}
+
+fun out(value: IntArray, prefix: String = ""): IntArray {
+    print(prefix)
+    println(value.asList())
+    return value
+}
+
+fun out(value: List<IntArray>, prefix: String = ""): List<IntArray> {
+    print(prefix)
+    println(value.map { it.asList() })
+    return value
+}
+
+fun readLines(filename: String): Stream<String> {
+    val resource = checkNotNull(ClassLoader.getSystemClassLoader().getResource(filename))
+    val path = Paths.get(resource.toURI())
+    return Files.lines(path)
+}
+
+fun readIntegers(filename: String): IntStream {
+    return readLines(filename).mapToInt(String::toInt)
+}
+
+fun readText(filename: String): String {
+    return readLines(filename).collect(Collectors.joining("\n"))
+}
+
+fun String.toListOfInts(): List<Int> {
+    return this.split(",").map { it.toInt() }
+}
