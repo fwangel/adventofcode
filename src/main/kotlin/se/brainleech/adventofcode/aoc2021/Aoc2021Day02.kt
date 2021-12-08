d7e0cfd3af7a5bd48dab19e25afe88bb6466507f
Fredrik Wangel <fredrik.wangel@gmail.com>
2021-12-08 01:39:26 +0100

    Small clean-up of 2021 day 2

--- src/main/kotlin/se/brainleech/adventofcode/aoc2021/Aoc2021Day02.kt
+++ src/main/kotlin/se/brainleech/adventofcode/aoc2021/Aoc2021Day02.kt
@@ -7,16 +7,15 @@ class Aoc2021Day02 {
     data class Command(val direction: String, val amount: Int)
 
     class Position(var horizontal: Int = 0, var depth: Int = 0, private var aim: Int = 0) {
-        fun apply(command: Command): Position {
+        fun apply(command: Command) {
             when (command.direction) {
                 "up" -> this.depth -= command.amount
                 "down" -> this.depth += command.amount
                 "forward" -> this.horizontal += command.amount
             }
-            return this
         }
 
-        fun applyWithAim(command: Command): Position {
+        fun applyWithAim(command: Command) {
             when (command.direction) {
                 "up" -> this.aim -= command.amount
                 "down" -> this.aim += command.amount
@@ -25,25 +24,23 @@ class Aoc2021Day02 {
                     this.depth += this.aim * command.amount
                 }
             }
-            return this
         }
+    }
+
+    private fun String.asCommand(): Command {
+        val (direction, amount) = this.split(" ")
+        return Command(direction, amount.toInt())
     }
 
     fun part1(input: Stream<String>): Int {
         val p = Position()
-        input
-            .map { it.split(' ') }
-            .map { Command(it[0], it[1].toInt()) }
-            .forEach { p.apply(it) }
+        input.forEach { p.apply(it.asCommand()) }
         return p.horizontal * p.depth
     }
 
     fun part2(input: Stream<String>): Int {
         val p = Position()
-        input
-            .map { it.split(' ') }
-            .map { Command(it[0], it[1].toInt()) }
-            .forEach { p.applyWithAim(it) }
+        input.forEach { p.applyWithAim(it.asCommand()) }
         return p.horizontal * p.depth
     }
 
