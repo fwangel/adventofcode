47f38765128ffc42f4fb10d654326f7b4ee606d0
Fredrik Wangel <fredrik.wangel@gmail.com>
2022-12-06 19:40:46 +0100

    Solved both parts of 2022 day 06

---
+++ src/main/kotlin/se/brainleech/adventofcode/aoc2022/Aoc2022Day06.kt
@@ -0,0 +1,45 @@
+package se.brainleech.adventofcode.aoc2022
+
+import se.brainleech.adventofcode.compute
+import se.brainleech.adventofcode.readLines
+import se.brainleech.adventofcode.verify
+
+class Aoc2022Day06 {
+
+    private fun process(input: String, packetSize: Int) : Int {
+        if (input.length < packetSize) return -1
+        return input
+            .toCharArray()
+            .asSequence()
+            .windowed(size = packetSize,  partialWindows = false)
+            .map { it.distinct() }
+            .withIndex()
+            .filter { it.value.size == packetSize }
+            .map { it.index + packetSize }
+            .first()
+    }
+
+    fun part1(input: String) : Int { return process(input, packetSize =  4) }
+    fun part2(input: String) : Int { return process(input, packetSize = 14) }
+}
+
+fun main() {
+    val solver = Aoc2022Day06()
+    val prefix = "aoc2022/aoc2022day06"
+    val testData = readLines("$prefix.test.txt").first()
+    val realData = readLines("$prefix.real.txt").first()
+
+    verify(7, solver.part1(testData))
+    verify(5, solver.part1("bvwbjplbgvbhsrlpgdmjqwftvncz"))
+    verify(6, solver.part1("nppdvjthqldpwncqszvftbrmjlhg"))
+    verify(10, solver.part1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"))
+    verify(11, solver.part1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"))
+    compute({ solver.part1(realData) }, "$prefix.part1 = ")
+
+    verify(19, solver.part2(testData))
+    verify(23, solver.part2("bvwbjplbgvbhsrlpgdmjqwftvncz"))
+    verify(23, solver.part2("nppdvjthqldpwncqszvftbrmjlhg"))
+    verify(29, solver.part2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"))
+    verify(26, solver.part2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"))
+    compute({ solver.part2(realData) }, "$prefix.part2 = ")
+}
